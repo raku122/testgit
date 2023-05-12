e579494caeabc6d8901b34f16ea6ea862f3f786b
raku122 <rakeshkumaracharya08@gmail.com>
2023-05-12 19:51:48 +0530

    print statement added

--- pythontest.py
+++ pythontest.py
@@ -1,1 +1,7 @@
 print("Hello this is a new python program running on GIT")
+
+name=input("what is your name")
+
+print("Thanks,welcome:",name)
+
+print("this is another statement with a \n whitespace in between")
